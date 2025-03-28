342bc0c35814cd36f4c8f6ea2945c3656e6982b1
liquidsquid1 <151129309+liquidsquid1@users.noreply.github.com>
2025-03-28 17:35:15 +0000

    feat: improve visuals

--- src/routes/hud/elements/BlockCounter.svelte
+++ src/routes/hud/elements/BlockCounter.svelte
@@ -1,33 +1,125 @@
 <script lang="ts">
-    import {listen} from "../../../integration/ws";
-    import {fly} from "svelte/transition";
-    import type {BlockCountChangeEvent} from "../../../integration/events";
-    import {mapToColor} from "../../../util/color_utils";
+  import { listen } from "../../../integration/ws";
+  import { blur, scale } from "svelte/transition";
+  import type { BlockCountChangeEvent, ClientPlayerDataEvent } from "../../../integration/events";
+  import type { PlayerData } from "../../../integration/types";
+  import { expoInOut } from "svelte/easing";
 
-    let count: number | undefined;
+  let count: number | undefined;
+  let playerData: PlayerData | null = null;
 
-    listen("blockCountChange", (data: BlockCountChangeEvent) => {
-        count = data.count;
-    });
+  let lastX = 0;
+  let lastZ = 0;
 
+  listen("blockCountChange", (data: BlockCountChangeEvent) => {
+      count = data.count;
+  });
+
+  listen("clientPlayerData", (event: ClientPlayerDataEvent) => {
+    if (playerData) {
+      lastX = playerData.position.x;
+      lastZ = playerData.position.z;
+    }
+    playerData = event.playerData;
+  });
+
+  function getProgressPercentage(value: number): number {
+      return (value % 64) / 64 * 100;
+  };
+
+  function roundToDecimal(value: number, decimal: number) {
+    return Math.round(value * Math.pow(10, decimal)) / Math.pow(10, decimal);
+  }
+
+  function getBPS(
+    lastX: number,
+    currentX: number,
+    lastZ: number,
+    currentZ: number,
+    tickrate: number
+  ): number {
+    const deltaX = currentX - lastX;
+    const deltaZ = currentZ - lastZ;
+
+    const distanceMoved = Math.sqrt(deltaX * deltaX + deltaZ * deltaZ);
+    const blocksPerSecond = distanceMoved * tickrate;
+
+    return blocksPerSecond;
+  }
 </script>
 
 {#if count !== undefined}
-    <div class="counter" style="color: {mapToColor(count)}" in:fly={{ y: -5, duration: 200 }}
-         out:fly={{ y: -5, duration: 200 }}>
-        {count}
-    </div>
+<div class="notification-wrapper">
+  <div class="glow" in:blur={{ duration: 500, easing: expoInOut }} out:blur={{ duration: 500, easing: expoInOut }}></div>
+  <div class="notification" in:blur={{ duration: 500, easing: expoInOut }} out:blur={{ duration: 500, easing: expoInOut }}>
+      <div class="header">
+          <span class="accent">scaffold</span>
+      </div>
+      <div class="info">
+          {count} blocks left
+          {#if playerData}
+            | {roundToDecimal(getBPS(lastX, playerData.position.x, lastZ, playerData.position.z, 20), 2)}m/s
+          {/if}
+      </div>
+      <div class="progress-bar">
+          <div class="progress" style="width: {getProgressPercentage(count)}%"></div>
+      </div>
+  </div>
+</div>
 {/if}
 
 <style lang="scss">
-  @use "../../../colors.scss" as *;
+@use "../../../colors.scss" as *;
 
-  .counter {
-    background-color: rgba($blockcounter-base-color, 0.68);
-    border-radius: 5px;
-    white-space: nowrap;
-    padding: 5px 8px;
-    font-weight: 500;
-    transform: translate(-100%);
+.notification-wrapper {
+  position: relative;
+  display: inline-block;
+
+  .glow {
+    position: absolute;
+    inset: -8px;
+    background: linear-gradient(to right, $flamingo, $mauve, $blue);
+    border-radius: 12px;
+    filter: blur(16px) opacity(50%);
+    opacity: 0.7;
+    z-index: -1 !important;
   }
+}
+
+.notification {
+  background-color: rgba($base, 0.8);
+  border-radius: 16px;
+  padding: 12px;
+  min-width: 200px;
+  color: rgba($text, 0.7);
+}
+
+.header span {
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  font-weight: 900;
+  background: linear-gradient(to right, $flamingo, $mauve, $blue);
+  background-clip: text;
+  color: transparent;
+}
+
+.info {
+  font-size: 14px;
+  margin-top: 5px;
+}
+
+.progress-bar {
+  margin-top: 10px;
+  background-color: rgba(255, 255, 255, 0.1);
+  border-radius: 5px;
+  height: 10px;
+  overflow: hidden;
+}
+
+.progress {
+  background-color: $accent;
+  height: 100%;
+  transition: ease width 0.1s;
+}
 </style>
--- src/routes/hud/elements/Scoreboard.svelte
+++ src/routes/hud/elements/Scoreboard.svelte
@@ -13,19 +13,21 @@
 </script>
 
 {#if scoreboard}
-    <div class="glow"></div>
-    <div class="scoreboard">
-        {#if scoreboard.header}
-            <div class="header">
-                <TextComponent fontSize={14} allowPreformatting={true} textComponent={scoreboard.header}/>
-            </div>
-        {/if}
-        <div class="entries">
-            {#each scoreboard.entries as {name, score}}
-                <div class="row">
-                    <TextComponent fontSize={14} allowPreformatting={true} textComponent={name}/>
+    <div class="scoreboard-wrapper">
+        <div class="glow"></div>
+        <div class="scoreboard">
+            {#if scoreboard.header}
+                <div class="header">
+                    <TextComponent fontSize={14} allowPreformatting={true} textComponent={scoreboard.header} />
                 </div>
-            {/each}
+            {/if}
+            <div class="entries">
+                {#each scoreboard.entries as { name, score }}
+                    <div class="row">
+                        <TextComponent fontSize={14} allowPreformatting={true} textComponent={name} />
+                    </div>
+                {/each}
+            </div>
         </div>
     </div>
 {/if}
@@ -33,24 +35,33 @@
 <style lang="scss">
   @use "../../../colors.scss" as *;
 
-  .glow {
-    position: absolute;
-    inset: -8px;
-    background: linear-gradient(to right, $flamingo, $mauve, $blue);
-    border-radius: 1rem;
-    filter: blur(12px) opacity(10%);
+  .scoreboard-wrapper {
+    position: relative;
+    display: inline-block;
+
+    .glow {
+      position: absolute;
+      inset: -8px;
+      background: linear-gradient(to right, $flamingo, $mauve, $blue);
+      border-radius: 12px;
+      filter: blur(16px) opacity(50%);
+      opacity: 0.7;
+      z-index: -1 !important;
+    }
   }
 
   .scoreboard {
+    z-index: 1 !important;
     width: max-content;
     border-radius: 12px;
     overflow: hidden;
     font-size: 14px;
     min-width: 200px;
+    border: 1px solid rgba($text, 0.3);
   }
 
   .entries {
-    background-color: rgba($base, 0.8);
+    background: rgba($base, 0.8);
     padding: 10px;
   }
 
@@ -64,5 +75,6 @@
     text-align: center;
     background-color: $crust;
     padding: 7px 10px;
+    font-weight: bold;
   }
 </style>
